2c6f38ed7211bd3c7d8259a983e6169afc8e2540
Quentin <clavier.quentin@gmail.com>
2023-07-23 17:12:33 +0200

    Create station.js

---
+++ src/classes/station.js
@@ -0,0 +1,41 @@
+class GasStation {
+    constructor(data) {
+        this.id = data.id;
+        this.cp = data.cp;
+        this.prix = this.parsePrices(data.prix);
+        this.ville = data.ville;
+        this.geom = data.geom;
+        this.carburants_indisponibles = this.parseCarburants(data.carburants_indisponibles);
+        this.carburants_disponibles = this.parseCarburants(data.carburants_disponibles);
+        this.adresse = data.adresse;
+    }
+
+    parsePrices(pricesString) {
+        const pricesArray = JSON.parse(pricesString);
+        return pricesArray.map(price => ({
+            nom: price["@nom"],
+            id: price["@id"],
+            maj: this.formatDate(price["@maj"]),
+            valeur: price["@valeur"]
+        }));
+    }
+    formatDate(dateString) {
+        const date = new Date(dateString);
+        const options = {
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric",
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit",
+            hour12: false,
+        };
+
+        return date.toLocaleString("en-GB", options);
+    }
+    parseCarburants(carburantsString) {
+        return carburantsString.split(";").map(carburant => carburant.trim());
+    }
+}
+
+export default GasStation;
